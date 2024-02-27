c92a8c2d18a1baf5d5fef5c8f49d914a2856eef7
Luís Campos <luismmc1997@hotmail.com>
2024-02-27 02:27:57 +0000

    Add library to convert transcriptions to .docx file

--- index.ts
+++ index.ts
@@ -1,34 +1,54 @@
-import { AssemblyAI, TranscribeParams, Transcript } from "assemblyai";
+import { AssemblyAI } from "assemblyai";
+import type { TranscribeParams, Transcript } from "assemblyai";
 import 'dotenv/config';
+import * as fs from 'fs';
+import type { Transcription } from './models/transcription';
+import * as PizZip from 'pizzip';
+import * as Docxtemplater from 'docxtemplater';
+import * as path from 'path';
 
 /**
  * Transcribe text from audio file
  *
- * @param filePath - Audio file to transcribe text
+ * @param inputAudioFile - Audio file to transcribe text (.mp3, .wav, etc...)
+ * @param outputDocxPath - Path for the genereated docx file (Ex.:  "C:\Users\user\Desktop\generated-doc.docx")
  */
-function transcribeTextFromAudio(filePath: string): void {
+async function transcribeTextFromAudio(inputAudioFile: string, outputDocxPath: string, witnessName?: string): Promise<void> {
+    if (!process.env.ASSEMBLY_API_KEY) {
+        console.error("Please set the ASSEMBLY_API_KEY environment variable");
+        return;
+    }
+
     const client = new AssemblyAI({
         apiKey: process.env.ASSEMBLY_API_KEY,
     });
 
     const data: TranscribeParams = {
-        audio: filePath,
+        audio: inputAudioFile,
         speaker_labels: true,
         language_code: "pt",
         speakers_expected: 4,
     };
 
-    client.transcripts.transcribe(data)
-        .then((transcript: Transcript) => {
-            for (const utterance of transcript.utterances) {
-                //TODO: Convert to .docx file
-                console.log(`${convertMillisecondsToTime(utterance.start)} Speaker ${utterance.speaker}: ${utterance.text}`);
+    try {
+        const transcript: Transcript = await client.transcripts.transcribe(data);
+        if (!transcript.utterances) {
+            console.log("It was not possible to transcribe any text.");
+            return;
+        }
+
+        const transcriptions: Transcription[] = transcript.utterances.map(utterance => {
+            return {
+                "timestamp": convertMillisecondsToTime(utterance.start),
+                "speaker": utterance.speaker,
+                "text": utterance.text
             }
-        })
-        .catch((err: Error) => {
-            console.error(`Something went wrong in the process of transcribing the text. Reason: ${err.message}`);
         });
 
+        generateDocxFile(transcriptions, path.parse(inputAudioFile).name, outputDocxPath);
+    } catch (err: any) {
+        console.error(`Something went wrong in the process of transcribing the text. Reason: ${err.message}`);
+    }
 };
 
 /**
@@ -46,10 +66,45 @@ function convertMillisecondsToTime(milliseconds: number): string {
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
-const filePath = process.argv[2];
-if (!filePath) {
-    console.error("Please provide the file path as a parameter.");
+/**
+ * Genreate a .docx file from the transcriptions
+ *
+ * @param transcriptions - List of all transcriptions
+ * @param inputAudioName - Name of the audio file (without file extension)
+ * @param outputDocxPath - Path for the genereated docx file (Ex.:  "C:\Users\user\Desktop\generated-doc.docx")
+ */
+function generateDocxFile(transcriptions: Transcription[], inputAudioName: string, outputDocxPath: string, witnessName?: string) {
+    const templateContent = fs.readFileSync('./template.docx', 'binary');
+
+    const zip = new PizZip(templateContent);
+    const doc = new Docxtemplater(zip, {
+        linebreaks: false,
+    });
+
+    doc.render({
+        fileName: inputAudioName,
+        witnessName: witnessName ?? "",
+        transcriptions,
+    });
+
+    const buff = doc.getZip().generate({
+        type: 'nodebuffer',
+        compression: "DEFLATE"
+    });
+
+    fs.writeFileSync(outputDocxPath, buff);
+}
+
+const audioFileToTranscribe = process.argv[2];
+const outputDocxPath = process.argv[3];
+if (!audioFileToTranscribe) {
+    console.error("Please provide the audio file path as a parameter 1.");
     process.exit(1);
 }
 
-transcribeTextFromAudio(filePath);
+if (!outputDocxPath) {
+    console.error("Please provide the docx path to save as a parameter 2.");
+    process.exit(1);
+}
+
+transcribeTextFromAudio(audioFileToTranscribe, outputDocxPath);
---
+++ models/transcription.ts
@@ -0,0 +1,5 @@
+export interface Transcription {
+    timestamp: string;
+    speaker: string;
+    text: string;
+}
